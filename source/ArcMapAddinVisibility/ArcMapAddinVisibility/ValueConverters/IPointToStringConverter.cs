5874e71e043e8b8b4600aea4881657aac71d775a
Joseph McCausland <jmccausland@esri.com>
2016-03-24 10:30:38 -0400

    Update IPointToString converter to use display Coordinate Type

--- source/ArcMapAddinVisibility/ArcMapAddinVisibility/ValueConverters/IPointToStringConverter.cs
+++ source/ArcMapAddinVisibility/ArcMapAddinVisibility/ValueConverters/IPointToStringConverter.cs
@@ -30,10 +30,46 @@ namespace ArcMapAddinVisibility
             if (value == null)
                 return "NA";
 
-            //TODO update with IConversionNotation
-
             var point = value as IPoint;
-            return string.Format("{0:0.0#####} {1:0.0#####}", point.Y, point.X);
+            if (point == null)
+                return "NA";
+
+            var result = string.Format("{0:0.0} {1:0.0}", point.Y, point.X);
+
+            if (point.SpatialReference == null)
+                return result;
+
+            var cn = point as IConversionNotation;
+            if (cn != null)
+            {
+                switch (ArcMapAddinVisibility.ViewModels.TabBaseViewModel.AddInConfig.DisplayCoordinateType)
+                {
+                    case CoordinateTypes.DD:
+                        result = cn.GetDDFromCoords(6);
+                        break;
+                    case CoordinateTypes.DDM:
+                        result = cn.GetDDMFromCoords(4);
+                        break;
+                    case CoordinateTypes.DMS:
+                        result = cn.GetDMSFromCoords(2);
+                        break;
+                    case CoordinateTypes.GARS:
+                        result = cn.GetGARSFromCoords();
+                        break;
+                    case CoordinateTypes.MGRS:
+                        result = cn.CreateMGRS(5, true, esriMGRSModeEnum.esriMGRSMode_Automatic);
+                        break;
+                    case CoordinateTypes.USNG:
+                        result = cn.GetUSNGFromCoords(5, true, true);
+                        break;
+                    case CoordinateTypes.UTM:
+                        result = cn.GetUTMFromCoords(esriUTMConversionOptionsEnum.esriUTMAddSpaces | esriUTMConversionOptionsEnum.esriUTMUseNS);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return result;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
