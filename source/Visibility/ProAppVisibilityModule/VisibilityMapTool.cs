9fb02e406e6100295a2fa5aaf9a7b50b82101422
Joseph McCausland <jmccausland@esri.com>
2016-04-12 13:39:42 -0400

    Add Pro AddIn with Dockpane, prop button and map tool

---
+++ source/Visibility/ProAppVisibilityModule/VisibilityMapTool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+
+namespace ProAppVisibilityModule
+{
+    internal class VisibilityMapTool : MapTool
+    {
+        protected override Task OnToolActivateAsync(bool active)
+        {
+            return base.OnToolActivateAsync(active);
+        }
+
+        protected override void OnToolMouseDown(MapViewMouseButtonEventArgs e)
+        {
+            base.OnToolMouseDown(e);
+        }
+    }
+}
