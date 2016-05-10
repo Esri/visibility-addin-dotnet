aee8cf8c801211550e2598e32dc12ca255e012eb
Dan Hagens <danhagens@gmail.com>
2016-05-10 11:52:32 -0400

    Added ArcMapAddinVisibility.Tests project

---
+++ source/Visibility/ArcMapAddinVisibility.Tests/ArcMapAddinVisibilityTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArcMapAddinVisibility.Tests
+{
+    [TestClass]
+    public class ArcMapAddinVisibilityTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
