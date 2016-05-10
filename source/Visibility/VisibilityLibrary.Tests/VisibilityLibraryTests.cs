2e908608e44588e5c0382ece3e78f4c2e8eb6451
Dan Hagens <danhagens@gmail.com>
2016-05-10 10:31:48 -0400

    Renamed unit test files to VisibilityLibrary.Tests

--- source/Visibility/VisibilityLibrary.Tests/VisibilityLibraryTests.cs
+++ source/Visibility/VisibilityLibrary.Tests/VisibilityLibraryTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace VisibilityLibrary.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class VisibilityLibraryTests
     {
         [TestMethod]
         public void TestMethod1()
