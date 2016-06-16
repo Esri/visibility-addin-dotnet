0ca1a071f47938ac77aa30a87f3f8ba12f30b066
Joseph McCausland <jmccausland@esri.com>
2016-06-16 12:56:17 -0400

    add ArcMap snapping support to map point tool

--- source/Visibility/ArcMapAddinVisibility/MapPointTool.cs
+++ source/Visibility/ArcMapAddinVisibility/MapPointTool.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using ESRI.ArcGIS.esriSystem;
 using ESRI.ArcGIS.Carto;
 using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Controls;
 using VisibilityLibrary.Helpers;
 using VisibilityLibrary;
 
@@ -11,6 +13,10 @@ namespace ArcMapAddinVisibility
 {
     public class MapPointTool : ESRI.ArcGIS.Desktop.AddIns.Tool
     {
+        ISnappingEnvironment m_SnappingEnv;
+        IPointSnapper m_Snapper;
+        ISnappingFeedback m_SnappingFeedback;
+
         public MapPointTool()
         {
         }
@@ -18,6 +24,18 @@ namespace ArcMapAddinVisibility
         protected override void OnUpdate()
         {
             Enabled = ArcMap.Application != null;
+        }
+
+        protected override void OnActivate()
+        {
+			//Get the snap environment and initialize the feedback
+			UID snapUID = new UID();
+
+			snapUID.Value = "{E07B4C52-C894-4558-B8D4-D4050018D1DA}";
+			m_SnappingEnv = ArcMap.Application.FindExtensionByCLSID(snapUID) as ISnappingEnvironment;
+			m_Snapper = m_SnappingEnv.PointSnapper;
+			m_SnappingFeedback = new SnappingFeedbackClass();
+			m_SnappingFeedback.Initialize(ArcMap.Application, m_SnappingEnv, true);
         }
 
         protected override void OnMouseDown(ESRI.ArcGIS.Desktop.AddIns.Tool.MouseEventArgs arg)
@@ -31,6 +49,12 @@ namespace ArcMapAddinVisibility
                 IActiveView activeView = ArcMap.Document.FocusMap as IActiveView;
 
                 var point = activeView.ScreenDisplay.DisplayTransformation.ToMapPoint(arg.X, arg.Y) as IPoint;
+                ISnappingResult snapResult = null;
+                //Try to snap the current position
+                snapResult = m_Snapper.Snap(point);
+                m_SnappingFeedback.Update(null, 0);
+                if (snapResult != null)
+                    point = snapResult.Location;
 
                 Mediator.NotifyColleagues(Constants.NEW_MAP_POINT, point);
             }
@@ -42,6 +66,12 @@ namespace ArcMapAddinVisibility
             IActiveView activeView = ArcMap.Document.FocusMap as IActiveView;
 
             var point = activeView.ScreenDisplay.DisplayTransformation.ToMapPoint(arg.X, arg.Y) as IPoint;
+            ISnappingResult snapResult = null;
+            //Try to snap the current position
+            snapResult = m_Snapper.Snap(point);
+            m_SnappingFeedback.Update(snapResult, 0);
+            if (snapResult != null)
+                point = snapResult.Location;
 
             Mediator.NotifyColleagues(Constants.MOUSE_MOVE_POINT, point);
         }
