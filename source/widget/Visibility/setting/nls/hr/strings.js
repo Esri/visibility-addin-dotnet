0d560b0cfb1121c4eddd352d95b7ef96dbb4394a
Anthony Giles <ad_giles@hotmail.com>
2018-03-02 15:58:03 +0000

    Updates to vis widget for UI changes

---
+++ source/widget/Visibility/setting/nls/hr/strings.js
@@ -0,0 +1,7 @@
+define({
+  "taskUrl": "Č_Viewshed Service URL_______ž",
+  "serviceURLPlaceholder": "Č_Please input Viewshed Geoprocessing task URL______________ž",
+  "setTask": "Č_Set__ž",
+  "setTaskTitle": "Č_Set Viewshed Geoprocessing Task__________ž",
+  "serviceURLError": "Č_The Geoprocessing task your are trying to configure with this widget is not valid. Please select a valid Viewshed Geoprocessing task URL__________________________________________ž"
+});
