f4dcd21fd91e764647487548423ad671e80958fb
SaiP <Sai.Phaneendra@cybertech.com>
2018-08-09 15:29:48 +0530

    Removed commented code

--- source/addins/ProAppVisibilityModule/ViewModels/ProLOSBaseViewModel.cs
+++ source/addins/ProAppVisibilityModule/ViewModels/ProLOSBaseViewModel.cs
@@ -1075,20 +1075,9 @@ namespace ProAppVisibilityModule.ViewModels
                 .Where(lyr => lyr.Name == selectedLayerName && lyr.ShapeType == esriGeometryType.esriGeometryPoint).FirstOrDefault();
                 var cursor = layer.GetFeatureClass().Search();
 
-                //define specific color for observer and target
-                //SimpleMarkerStyle ms = SimpleMarkerStyle.Circle;
-                //CIMColor color = ColorFactory.Instance.BlueRGB;
-
-                //if (tag == "target")
-                //{
-                //    ms = SimpleMarkerStyle.Square;
-                //    color = ColorFactory.Instance.RedRGB;
-                //}
-
                 while (cursor.MoveNext())
                 {
                     var point = (MapPoint)cursor.Current["Shape"];
-                    //var guid = await AddGraphicToMap(point, color, true, 5.0, markerStyle: ms, tag: tag);
                     var addInPoint = new AddInPoint { Point = point, GUID = Guid.NewGuid().ToString() };
                     var objectId = -1;
                     var FID = -1;
